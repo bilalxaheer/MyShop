6b14fbdde52f0a8857c25b0018c6c93ca01dc408
Bilal Zaheer <bilalxaheer@gmail.com>
2021-07-15 18:43:07 +0500

    Admin Pages Secure Process

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -9,6 +9,7 @@ using System.IO;
 
 namespace MyShop.WebUI.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class ProductManagerController : Controller
     {
         IRepository<Product> context;
